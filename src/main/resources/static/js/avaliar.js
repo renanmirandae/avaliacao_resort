a3d21c744f29bfb2d9f1c9a816e9e38c38f362e3
renanmirandae <renanmirandae@gmail.com>
2022-09-24 15:46:12 -0300

    Criação da estrutura de pastas da view da aplicação. Elaboração do front-end da página de postar avaliação

---
+++ src/main/resources/static/js/avaliar.js
@@ -0,0 +1,39 @@
+let urlAvaliar = "/avaliar";
+let nomeText = document.getElementById("nome");
+let comentarioText = document.getElementById("comentario");
+let avaliacaoSelect = document.getElementById("avaliacao");
+let botaoAvaliar = document.getElementById("bt_avaliar");
+
+let nome, comentario, avaliacao, body;
+
+botaoAvaliar.addEventListener('click', (event) =>
+{
+    nome = nomeText.value == "" ? "Anônimo" : nomeText.value
+    if(comentarioText.value.length <10 )
+    {
+        window.alert("Seu comentário deve conter no minimo 10 caracteres");
+    }else{
+        comentario = comentarioText.value;
+        avaliacao = avaliacaoSelect.value;
+        body = {nome: nome, comentario: comentario, avaliacao: avaliacao};
+        postarAvaliacao(body);
+    }
+    event.preventDefault();
+});
+
+//Postar Avaliação
+const postarAvaliacao = 
+function(body)
+{
+    fetch(urlAvaliar,
+        {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            
+            body: JSON.stringify(body)
+        })
+        .then(response => {console.log(response)})
+        .catch(error => {})
+};
